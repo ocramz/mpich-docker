f8e245aab46478d389dd937ff566b1c7a5137ddc
ocramz <zocca.marco@gmail.com>
2016-06-20 06:11:38 +0200

    typos

--- bin/start.sh
+++ bin/start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-USER=mpiuser1
+# # `mpiuser` already exists at this stage
+# USER=mpiuser1
 
-# sshd daemon runs in /var/run/sshd 
+# # sshd daemon runs in /var/run/sshd 
 
-# Add user and generate a random password with 12 characters that includes at least one capital letter and number.
-DOCKER_PASSWORD=`pwgen -c -n -1 12`
-echo User: mpiuser1 Password: $DOCKER_PASSWORD
-DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+# # Add user and generate a random password with 12 characters that includes at least one capital letter and number.
+# DOCKER_PASSWORD=`pwgen -c -n -1 12`
+# echo User: mpiuser1 Password: $DOCKER_PASSWORD
+# DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
 
-useradd -m -d /home/${USER} -s /bin/bash -p $DOCKER_ENCRYPYTED_PASSWORD ${USER}
-echo Added user ${USER} with encrypted pwd
+# useradd -m -d /home/${USER} -s /bin/bash -p $DOCKER_ENCRYPYTED_PASSWORD ${USER}
+# echo Added user ${USER} with encrypted pwd
 
-sed -Ei "s/adm:x:4:/${USER}:x:4:${USER}/" /etc/group
-echo Added user ${USER} to group
+# sed -Ei "s/adm:x:4:/${USER}:x:4:${USER}/" /etc/group
+# echo Added user ${USER} to group
 
-# adduser docker sudo
+# # adduser docker sudo
 
 
 # Start the ssh service
