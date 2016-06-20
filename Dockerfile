97692699370bc45b1436077fccc2b3d202576181
ocramz <zocca.marco@gmail.com>
2016-06-20 05:44:23 +0200

    added start script

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,14 @@ RUN apt-get update && \
     apt-get purge && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
+# add user `mpiuser`
+RUN useradd -m mpiuser
+
+
+
+# Copy the files into the container
+ADD /bin /bin
+
+EXPOSE 22
+
+CMD ["/bin/bash", "/bin/start.sh"]
---
+++ bin/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USER=mpiuser
+
+# Create the directory needed to run the sshd daemon
+mkdir /var/run/sshd 
+
+# Add docker user and generate a random password with 12 characters that includes at least one capital letter and number.
+DOCKER_PASSWORD=`pwgen -c -n -1 12`
+echo User: docker Password: $DOCKER_PASSWORD
+DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'
+`
+useradd -m -d /home/${USER} -s /bin/bash -p $DOCKER_ENCRYPYTED_PASSWORD ${USER}
+
+
+sed -Ei "s/adm:x:4:/${USER}:x:4:${USER}/" /etc/group
+
+# adduser docker sudo
+
+
+# Start the ssh service
+/usr/sbin/sshd -D
